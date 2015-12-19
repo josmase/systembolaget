b58dbe50c0e1c640b518fd509bb65ff376de91c3
josmase <josnase@GMAIL.COM>
2015-12-19 17:10:10 +0100

    "orderBy" now working

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,12 +9,17 @@
  */
 angular.module('systembolagetApp')
   .controller('MainCtrl', function ($scope, $http) {
-
+  $scope.sort = "Namn";
     loadData();
-
-    /**Gets data from axml file and puts it into $scope*/
+    $scope.predicate = 'age';
+    $scope.reverse = true;
+    $scope.order = function(predicate) {
+      $scope.reverse = ($scope.predicate === predicate) ? !$scope.reverse : false;
+      $scope.predicate = predicate;
+    };
+    /**Gets data from xml file and puts it into $scope*/
     function loadData() {
-      $http.get("./scripts/controllers/articles.xml"
+      $http.get("./scripts/controllers/sak.xml"
       ).
       success(function (data) {
         $scope.articles = x2js.xml_str2json(data);
