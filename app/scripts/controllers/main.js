d6f50c3d75b833cdfba0ab63923a930c1bb7b7f7
josmase <josnase@gmail.com>
2015-12-21 14:12:50 +0100

    Working api

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,53 +9,75 @@
  */
 angular.module('systembolagetApp')
   .controller('MainCtrl', function ($scope, $http) {
-    $scope.sort = "Namn";
+
+
+    $scope.alcohol = {
+      min: 0,
+      max: 100
+    };
+    $scope.price = {
+      min: 0,
+      max: 999999
+    };
+    $scope.apk = {
+      min: 2,
+      max: 200
+    };
+
+
     loadData();
-    $scope.predicate = 'namn';
+    $scope.sort = "Name";
+    $scope.predicate = 'name';
     $scope.reverse = true;
     $scope.order = function (predicate) {
       $scope.reverse = ($scope.predicate === predicate) ? !$scope.reverse : false;
       $scope.predicate = predicate;
     };
+
     /**Gets data from xml file and puts it into $scope*/
     function loadData() {
-
-      $http.get('https://karlroos-systemet.p.mashape.com/product?alcohol_from=0.4&alcohol_to=1&apk_from=0&apk_to=9000&price_from=0&price_per_liter_from=0&price_per_liter_to=900000&price_to=900000',
-        {
-          headers: {'X-Mashape-Key': 'OZ9i1HXl2Hmshk5RuUK0N983D9GXp1MsAFnjsnpdlRfMKb7V6F', 'Accept' : 'application/json' }
-        })
-      .
-      success(function (data) {
+      $http.get('https://karlroos-systemet.p.mashape.com/product?' +
+        'alcohol_from=' + ($scope.alcohol.min / 100) +
+        '&alcohol_to=' + ($scope.alcohol.max / 100) +
+        '&apk_from=' + $scope.apk.min +
+        '&apk_to=' + $scope.apk.max +
+        '&price_from=' + $scope.price.min +
+        '&price_per_liter_from=0' +
+        '&price_per_liter_to=900000' +
+        '&price_to=' + $scope.price.max
+        , {
+          headers: {'X-Mashape-Key': 'OZ9i1HXl2Hmshk5RuUK0N983D9GXp1MsAFnjsnpdlRfMKb7V6F', 'Accept': 'application/json'}
+        }).success(function (data) {
         console.log(data);
         $scope.articles = data;
       })
     }
 
-  }).filter('alcohol', function() {
-  return function(input, object) {
-    var filteredAmount = [];
-    angular.forEach(input, function(item) {
-      if (Math.floor(item.Alkoholhalt) <= (object.max || 0) &&
-        Math.floor(item.Alkoholhalt) >= (object.min || 0)) {
+  }).filter('alcohol', function () {
+    return function (input, object) {
+      var filteredAmount = [];
+      angular.forEach(input, function (item) {
+        if (Math.floor(item.Alkoholhalt) <= (object.max || 0) &&
+          Math.floor(item.Alkoholhalt) >= (object.min || 0)) {
 
-        filteredAmount.push(item);
-      }
-    });
+          filteredAmount.push(item);
+        }
+      });
 
-    return filteredAmount.length > 0 ? filteredAmount : [];
-  };
-})
-  .filter('priceMatch', function() {
-  return function(input, object) {
-    var filteredAmount = [];
-    angular.forEach(input, function(item) {
-      if (Math.floor(item.Prisinklmoms) <= (object.max || 0) &&
+      return filteredAmount.length > 0 ? filteredAmount : [];
+    };
+  })
+  .filter('priceMatch', function () {
+    return function (input, object) {
+      var filteredAmount = [];
+      angular.forEach(input, function (item) {
+        if (Math.floor(item.Prisinklmoms) <= (object.max || 0) &&
           Math.floor(item.Prisinklmoms) >= (object.min || 0)) {
 
-        filteredAmount.push(item);
-      }
-    });
+          filteredAmount.push(item);
+        }
+      });
 
-    return filteredAmount.length > 0 ? filteredAmount : [];
-  };
-});
+      return filteredAmount.length > 0 ? filteredAmount : [];
+    };
+  });
