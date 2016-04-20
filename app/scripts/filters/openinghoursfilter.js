f6bf0aa44c23fe3b8a806aa605d55c4ee66a3635
jonas <t4.jonas.lundberg@umea.nti.se>
2016-04-20 12:39:15 +0200

    filter unreadable opening hours data to human readable opening hours

---
+++ app/scripts/filters/openinghoursfilter.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/**
+ * @ngdoc filter
+ * @name systembolagetApp.filter:openingHoursFilter
+ * @function
+ * @description
+ * # openingHoursFilter
+ * Filter in the systembolagetApp.
+ */
+angular.module('systembolagetApp')
+  .filter('openingHoursFilter', function () {
+    return function (input, index) {
+      var regex = /([0-9][^;]{9});([0-9].{4});([0-9].{4});;;(.[^;]*);/g;
+      var matched;
+      if (typeof index === 'number') {
+        matched = matchAll(input, regex)[index];
+        if (matched[4] === '-') {
+          return 'Stängt'
+        }
+        else if (matched[4] == 0) {
+          return matched[2] + " - " + matched[3];
+        }
+        return matched[4];
+      }
+      else {
+        matched = matchAll(input, regex);
+        return matched;
+      }
+    };
+    function matchAll(input, regexp) {
+      var matches = [];
+      input.replace(regexp, function () {
+        var arr = ([]).slice.call(arguments, 0);
+        var extras = arr.splice(-2);
+        arr.index = extras[0];
+        arr.input = extras[1];
+        matches.push(arr);
+      });
+      return matches.length ? matches : null;
+    }
+
+
+  });
