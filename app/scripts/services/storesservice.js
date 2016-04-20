b86816b4190f3f91ea755b82f92c22a10308ac2a
jonas <t4.jonas.lundberg@umea.nti.se>
2016-04-20 09:44:46 +0200

    handle interaction with related to stores

---
+++ app/scripts/services/storesservice.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name systembolagetApp.storesService
+ * @description
+ * # storesService
+ * Service in the systembolagetApp.
+ */
+angular.module('systembolagetApp')
+  .service('storesService', function ($http) {
+      var server = 'http://localhost:8000/api/';
+      var functions = {};
+
+      function handleSuccess(result) {
+        return result;
+      }
+
+      function handleError(error) {
+        return {success: false, error: error}
+      }
+
+      function getStoresLatLon(position) {
+        console.log(position);
+        return $http({
+          method: 'GET',
+          url: server + 'stores',
+          headers: {'Accept': 'application/json'},
+          params: {
+            lat: position.latitude, lon: position.longitude
+          }
+        }).then(handleSuccess, handleError('kunde inte nå servern'));
+      }
+
+      functions.getStoresLatLon = getStoresLatLon;
+      return functions
+    }
+  );
